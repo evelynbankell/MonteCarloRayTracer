551175121e63cbf7d207b67fbf038cf163e043ef
evelynbankell <evelyn@bankell.se>
2020-09-15 10:56:20 +0200

    first commit

---
+++ raytracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  raytracer
+//
+//  Created by Evelyn Bankell on 2020-09-15.
+//  Copyright © 2020 Evelyn Bankell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
